764d381bb32700c9cbfd81519cb0a443c339d47f
i-Lucas <novo.contato.lucas@gmail.com>
2022-05-18 07:32:50 -0300

    change max limit caracter for product name

--- src/schemas/products.js
+++ src/schemas/products.js
@@ -2,7 +2,7 @@ import joi from 'joi';
 
 export const NewProductSchema = joi.object({
 
-    productName: joi.string().min(5).max(15).required(),
+    productName: joi.string().min(5).max(25).required(),
     productPrice: joi.number().required(),
     productDescription: joi.string().min(5).required()
 });
